630e4d3173f93349649d45182e2551149040e8ce
Ivan Towlson <ivan.towlson@microsoft.com>
2017-03-06 16:23:59 +1300

    Initial heuristics for determining the kind of runtime in the Docker container (so we can work out how to attach a debugger)

---
+++ analyser.js
@@ -0,0 +1,65 @@
+function analyse(dockerfile) {
+    if (!dockerfile) {
+        return { succeeded: false };
+    }
+
+    // Analysis is entirely heuristic - it looks for characteristic text in the
+    // dockerfile, specifically EXPOSE and CMD elements
+
+    var succeeded = false;
+    var runtime = undefined;
+    var exposesService = false;
+    var seenEntryPoint = false;
+
+    var lines = dockerfile.split(/\r?\n/);
+    lines.forEach(function (line) {
+        if (line.startsWith("ENTRYPOINT ")) {
+            runtime = inferRuntime(line.substring(11));
+            if (runtime) {
+                succeeded = true;
+            }
+            seenEntryPoint = true;
+        }
+        if (line.startsWith("CMD ") && !seenEntryPoint) {
+            runtime = inferRuntime(line.substring(4));
+            if (runtime) {
+                succeeded = true;
+            }
+        }
+        if (line.startsWith("EXPOSE ")) {
+            exposesService = true;
+        }
+    });
+
+    return { succeeded: succeeded, runtime: runtime, exposesService: exposesService };
+}
+
+function inferRuntime(dockerCmd) {
+    // Possible forms:
+    // ["executable", "param1", "param2"] (exec form)
+    // command param1 param2 (shell form)
+    var items = undefined;
+    if (dockerCmd.startsWith('[')) {
+        // exec form
+        items = JSON.parse(dockerCmd);
+    } else {
+        items = dockerCmd.split(/\s+/)
+    }
+
+    if (items.length < 1) {
+        return null;
+    }
+
+    var command = items[0].trim();
+
+    if (command === "node") {
+        return 'node';
+    }
+    if (command === "python") {
+        return 'python'
+    }
+
+    return null;
+}
+
+exports.analyse = analyse;
---
+++ test/analyser.test.js
@@ -0,0 +1,51 @@
+// The module 'assert' provides assertion methods from node
+var assert = require('assert');
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+var vscode = require('vscode');
+var analyser = require('../analyser');
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Analyser Tests", function() {
+
+    test("Analyser identifies Node.js dockerfiles", function() {
+        var analysis = analyser.analyse("FROM node:6.9.2\nEXPOSE 8080\nCOPY server.js .\nCMD node server.js");
+        assert.equal('node', analysis.runtime);
+    });
+
+    test("Analyser identifies Python dockerfiles", function() {
+        var analysis = analyser.analyse('FROM  python:3\nEXPOSE  80\nCMD ["python", "-m", "http.server"]');
+        assert.equal('python', analysis.runtime);
+    });
+
+    test("Analyser does not identify unknown runtimes", function() {
+        var analysis = analyser.analyse('FROM befunge:0.1.0\nCMD befunge clever.bf');
+        assert.equal(false, analysis.succeeded);
+    });
+
+    test("Analyser identifies exposed Web service", function() {
+        var analysis = analyser.analyse("FROM node:6.9.2\nEXPOSE 8080\nCOPY server.js .\nCMD node server.js");
+        assert.equal(true, analysis.exposesService);
+    });
+
+    test("Analyser identifies non-exposed commands", function() {
+        var analysis = analyser.analyse("FROM node:6.9.2\nCOPY service.js .\nCMD node service.js");
+        assert.equal(false, analysis.exposesService);
+    });
+
+    test("Analyser infers runtime from ENTRYPOINT (exec style)", function() {
+        var analysis = analyser.analyse('FROM node:6.9.2\nEXPOSE 8080\nCOPY server.js .\nENTRYPOINT ["node", "server.js"]');
+        assert.equal('node', analysis.runtime);
+    });
+
+    test("Analyser infers runtime from ENTRYPOINT (shell style)", function() {
+        var analysis = analyser.analyse('FROM node:6.9.2\nEXPOSE 8080\nCOPY server.js .\nENTRYPOINT node server.js');
+        assert.equal('node', analysis.runtime);
+    });
+
+    test("Analyser infers runtime from ENTRYPOINT and ignores CMD default args", function() {
+        var analysis = analyser.analyse('FROM node:6.9.2\nEXPOSE 8080\nCOPY server.js .\nENTRYPOINT node\nCMD ["python"]');
+        assert.equal('node', analysis.runtime);
+    });
+});
