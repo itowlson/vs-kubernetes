f159148f7e8a7c15159025b2682e24c49d8dc18f
Ivan Towlson <ivan.towlson@microsoft.com>
2017-04-27 11:24:14 +1200

    Tidying the fast explainer and removing dead code

--- src/explainer.ts
+++ src/explainer.ts
@@ -4,286 +4,11 @@ import * as k8s from 'k8s';
 import * as pluralize from 'pluralize';
 import * as kubeconfig from './kubeconfig';
 
-export function formatExplain(rawText) {
-    if (!rawText) {
-        return rawText;
-    }
-
-    var lines = rawText.replace(/\r\n/g, '\n').split('\n');
-
-    if (rawText.startsWith('FIELD')) {
-        return formatField(lines);
-    }
-
-    if (rawText.startsWith('DESCRIPTION')) {
-        return formatDescription(lines);
-    }
-
-    if (rawText.startsWith('RESOURCE')) {
-        return formatResource(lines);
-    }
-
-    return {
-        'language': "json",
-        'value': rawText
-    };
-}
-
-function formatField(lines) {
-    /*
-    source:
-
-    FIELD: name <type>
-
-    DESCRIPTION:
-        first line
-        second line
-
-    desired output:
-
-    **FIELD:** name <type>
-
-    **DESCRIPTION:**
-    first line
-    second line
-    */
-
-    var formattedLines = lines.map(function (line) {
-        var formatted = emboldenPrefix(line);
-        formatted = removeLeading(formatted);
-        return formatted;
-    }, this);
-
-    return formattedLines.join('\n')
-}
-
-function formatDescription(lines) {
-    /*
-    source:
-
-    DESCRIPTION:
-    first line
-    second line
-
-    FIELDS:
-        field1 <type>
-        first line
-        second line
-
-        field2 <type>
-        first line
-        second line
-
-    desired output:
-
-    **DESCRIPTION:**
-    first line
-    second line
-
-    **FIELDS:**
-
-    **field1** <type>
-
-    first line
-    second line
-
-    **field2** <type>
-
-    first line
-    second line
-    */
-
-    var parserState = 'init';
-    var formattedLines = [];
-    let formatted;
-
-    while (lines.length > 0) {
-        var line = lines.shift();
-        switch (parserState) {
-            case 'init':
-                formatted = emboldenPrefix(line);
-                formattedLines.push(formatted);
-                if (formatted.startsWith('**FIELD')) {
-                    formattedLines.push("");
-                    parserState = 'fields-none';
-                }
-                break;
-            case 'fields-none':
-                formatted = removeLeading(line);
-                formatted = emboldenFieldName(formatted);
-                formattedLines.push(formatted);
-                if (formatted.startsWith('**')) {
-                    parserState = 'field-first';
-                }
-                break;
-            case 'field-first':
-                if (line.length === 0) {
-                    break;
-                }
-                formatted = removeLeading(line);
-                formattedLines.push("");
-                formattedLines.push(formatted);
-                parserState = 'field-rest';
-                break;
-            case 'field-rest':
-                if (line.length === 0) {
-                    parserState = 'fields-none';
-                    formattedLines.push(line);
-                    break;
-                }
-                formatted = removeLeading(line);
-                formattedLines.push(formatted);
-                break;
-        }
-    }
-
-    return formattedLines.join('\n');
-}
-
-function formatResource(lines) {
-    /*
-    source:
-
-    RESOURCE: name <type>
-
-    DESCRIPTION:
-         first summary line
-         second summary line
-
-        first line
-        second line
-
-    FIELDS:
-       field1 <type>
-         first line
-         second line
-
-       field2 <type>
-         first line
-         second line
-
-    desired output:
-
-    **RESOURCE:** name <type>
-
-    **DESCRIPTION:**
-    first summary line
-    second summary line
-
-    first line
-    second line
-
-    **FIELDS:**
-
-    **field1** <type>
-
-    first line
-    second line
-
-    **field2** <type>
-
-    first line
-    second line
-    */
-
-    var parserState = 'init';
-    var formattedLines = [];
-    let formatted;
-
-    while (lines.length > 0) {
-        var line = lines.shift();
-        switch (parserState) {
-            case 'init':
-                formatted = emboldenPrefix(line);
-                formattedLines.push(formatted);
-                if (formatted.startsWith('**DESCRIPTION')) {
-                    parserState = 'description-body';
-                }
-                if (formatted.startsWith('**FIELD')) {
-                    formattedLines.push("");
-                    parserState = 'fields-none';
-                }
-                break;
-            case 'description-body':
-                formatted = emboldenPrefix(line);
-                formatted = removeLeading(formatted);
-                formattedLines.push(formatted);
-                if (formatted.startsWith('**FIELD')) {
-                    formattedLines.push("");
-                    parserState = 'fields-none';
-                }
-                break;
-            case 'fields-none':
-                formatted = removeLeading(line);
-                formatted = emboldenFieldName(formatted);
-                formattedLines.push(formatted);
-                if (formatted.startsWith('**')) {
-                    parserState = 'field-first';
-                }
-                break;
-            case 'field-first':
-                if (line.length === 0) {
-                    break;
-                }
-                formatted = removeLeading(line);
-                formattedLines.push("");
-                formattedLines.push(formatted);
-                parserState = 'field-rest';
-                break;
-            case 'field-rest':
-                if (line.length === 0) {
-                    parserState = 'fields-none';
-                    formattedLines.push(line);
-                    break;
-                }
-                formatted = removeLeading(line);
-                formattedLines.push(formatted);
-                break;
-        }
-    }
-
-    return formattedLines.join('\n');
-}
-
-function emboldenPrefix(line) {
-    if (!line) {
-        return line;
-    }
-    var prefixes = ['FIELD:', 'FIELDS:', 'DESCRIPTION:', 'RESOURCE:'];
-    prefixes.forEach(function (prefix) {
-        if (line.startsWith(prefix)) {
-            line = '**' + prefix + '**' + line.substring(prefix.length);
-        }
-    });
-    return line;
-}
-
-function emboldenFieldName(line) {
-    if (!line) {
-        return line;
-    }
-    var parse = line.match(/^(\w+)\s+\<(\[\])?\w+\>(\s+-required-)?$/);
-    if (parse) {
-        line = '**' + parse[1] + '**' + line.substring(parse[1].length)
-        if (parse[3]) {
-            line = line.replace('-required-', '**[required]**')
-        }
-    }
-    return line;
-}
-
-function removeLeading(line) {
-    if (!line) {
-        return line;
-    }
-    return line.replace(/^\s+/, '')
-}
-
-export function readSwagger() {
+export function readSwagger() : Promise<any> {
     return kubeconfig.readKubectlConfig().then(kc => readSwaggerCore(kc));
 }
 
-function readSwaggerCore(kc : kubeconfig.KubeConfig) {
+function readSwaggerCore(kc : kubeconfig.KubeConfig) : Promise<any> {
     return new Promise(function (resolve, reject) {
         var kapi = k8s.api(apiCredentials(kc));
         kapi.get('swagger.json', function (err, data) {
@@ -296,7 +21,7 @@ function readSwaggerCore(kc : kubeconfig.KubeConfig) {
     });
 }
 
-export function readExplanation(swagger, fieldsPath : string) {
+export function readExplanation(swagger : any, fieldsPath : string) {
     var fields = fieldsPath.split('.');
     var kindName = fields.shift();
     var kindDef = findKindModel(swagger, kindName);
@@ -304,14 +29,14 @@ export function readExplanation(swagger, fieldsPath : string) {
     return text;
 }
 
-function findKindModel(swagger, kindName : string) : TypeModel {
+function findKindModel(swagger : any, kindName : string) : TypeModel {
     var v1def = findProperty(swagger.definitions, 'v1.' + kindName);
     var v1beta1def = findProperty(swagger.definitions, 'v1beta1.' + kindName);
     var kindDef = v1def || v1beta1def;
     return kindDef;
 }
 
-function chaseFieldPath(swagger, currentProperty : TypeModel, currentPropertyName : string, fields : string[]) {
+function chaseFieldPath(swagger : any, currentProperty : TypeModel, currentPropertyName : string, fields : string[]) {
 
     // What are our scenarios?
     // 1. (ex: Deployment.[metadata]): We are at the end of the chain and
@@ -396,7 +121,7 @@ function chaseFieldPath(swagger, currentProperty : TypeModel, currentPropertyNam
             var typeRefProperties = currentPropertyTypeInfo.properties;
             if (typeRefProperties) {
                 if (fields.length === 0) {
-                    return explainComplex2(currentPropertyName, currentProperty.description, currentPropertyTypeInfo.description, typeRefProperties);
+                    return explainComplex(currentPropertyName, currentProperty.description, currentPropertyTypeInfo.description, typeRefProperties);
                 } else {
                     var nextField = fields.shift();
                     var nextProperty = findProperty(typeRefProperties, nextField);
@@ -418,7 +143,7 @@ function chaseFieldPath(swagger, currentProperty : TypeModel, currentPropertyNam
         var properties = currentProperty.properties;
         if (properties) {
             if (fields.length === 0) {
-                return explainComplex(currentPropertyName, currentProperty.description, properties);
+                return explainComplex(currentPropertyName, currentProperty.description, undefined, properties);
             } else {
                 var nextField = fields.shift();
                 var nextProperty = findProperty(properties, nextField);
@@ -438,73 +163,20 @@ function explainOne(name : string, type : string, description : string) {
     return `**${name}** (${type})\n\n${description}`;
 }
 
-function explainComplex(name : string, description : string, children : any) {
+function explainComplex(name : string, description : string, typeDescription : string | undefined, children : any) {
     var ph = '';
     for (var p in children) {
         ph = ph + `**${p}** (${typeDesc(children[p])})\n\n${children[p].description}\n\n`;
     }
-    return `${name}: ${description}\n\n${ph}`;
-}
-
-function explainComplex2(name : string, description : string, typeDescription : string, children : any) {
-    var ph = '';
-    for (var p in children) {
-        ph = ph + `**${p}** (${typeDesc(children[p])})\n\n${children[p].description}\n\n`;
+    var typeDescriptionPara = '';
+    if (typeDescription) {
+        typeDescriptionPara = `\n\n${typeDescription}`;
     }
-    return `${name}: ${description}\n\n${typeDescription}\n\n${ph}`;
+    return `${name}: ${description}${typeDescriptionPara}\n\n${ph}`;
 }
 
 function explainError(header : string, error : string) {
     return `**${header}:** ${error}`;
-}
-
-function chaseFieldPath_old(swagger, startingFrom, name : string, fields : string[]) {
-    if (fields.length === 0) {
-        // startingFrom is the definition of the field - dump it
-        // (it may be a simple or complex type)
-        var props = startingFrom.properties;
-        if (props) {
-            var ph = '';
-            for (var p in props) {
-                ph = ph + `**${p}** (${typeDesc(props[p])})\n\n${props[p].description}\n\n`;
-            }
-            return `${name}: ${startingFrom.description}\n\n${ph}`;
-        } else {
-            // EXAMPLE: Deployment.metadata.creationTimestamp
-            // (but not generation oddly enough)
-            // LOOKS LIKE: $ref where the ref has { type: string } etc.
-            // instead of { properties : [...] }
-            var type = typeDesc(startingFrom);
-            return `**${name}** (${type})\n\n${startingFrom.description} BUT OH NO LOST MY CONTEXT WANT TO DESCRIBE PROPERTY NOT TYPE`;
-            //return "PRIMITIVE: " + JSON.stringify(startingFrom);
-        }
-    } else {
-        // we are at an interim stage of the chain: chase
-        // to the type of the next field in the traversal
-        var fieldName = fields.shift();
-        var props = startingFrom.properties;
-        var fieldDefn = findProperty(props, fieldName);
-        var fieldType = underlyingFieldType(fieldDefn);
-
-        if (fieldType) {
-            var typeDefnPath : string[] = fieldType.split('/');
-            typeDefnPath.shift();
-            var typeDefn = findTypeDefinition(swagger, typeDefnPath);
-            return chaseFieldPath_old(swagger, typeDefn, fieldName, fields);
-        } else {
-            if (fields.length === 0) {
-                return `**${fieldName}** (${fieldDefn.type})\n\n${fieldDefn.description}`;
-            } else {
-                // we're at a primitive type, but still have a path to
-                // traverse - error
-                // TODO: this can happen when you point to an element in
-                // a KVP collection (as those can be dynamic)
-                // this can be determined as type=object but no $ref
-                // EXAMPLE: Deployment.metadata.annotations.*
-                return `ERROR: terminal type ${startingFrom.name} with outstanding path from ${fieldName}`;
-            }
-        }
-    }
 }
 
 function typeDesc(p : Typed) {
@@ -535,7 +207,7 @@ function singularizeVersionedName(name : string) {
     return bits.join('.');
 }
 
-function findProperty(obj, name) {
+function findProperty(obj : any, name : string) {
     var n = (name + "").toLowerCase();
     for (var p in obj) {
         if ((p + "").toLowerCase() == n) {
@@ -550,12 +222,12 @@ function findProperty(obj, name) {
     }
 }
 
-function findTypeDefinition(swagger, typeDefnPath : string[]) : TypeModel {
+function findTypeDefinition(swagger : any, typeDefnPath : string[]) : TypeModel | undefined {
     var m = swagger;
     for (var p of typeDefnPath) {
         m = findProperty(m, p);
         if (!m) {
-            throw { errorCategory: 'unresolvableTypePath', message: `ERROR: undefined at ${m.name}->${p}` };
+            return undefined;
         }
     }
     return m;
--- test/extension.test.js
+++ test/extension.test.js
@@ -5,8 +5,6 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
-// The module 'assert' provides assertion methods from node
-var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 
@@ -19,28 +17,6 @@ var textassert = require('../out/test/textassert');
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
-    test("Fields are transformed correctly", function() {
-        assert.equal("**FIELD:** apiVersion", explainer.formatExplain("FIELD: apiVersion"));
-        assert.equal("**FIELD:** apiVersion\n\n**DESCRIPTION:**\nThe version\nof the API", explainer.formatExplain("FIELD: apiVersion\n\nDESCRIPTION:\n    The version\n    of the API"));
-    });
-
-    test("Descriptions are transformed correctly", function() {
-        assert.equal("**DESCRIPTION:**\nKind does thing", explainer.formatExplain("DESCRIPTION:\nKind does thing"));
-        assert.equal("**DESCRIPTION:**\nKind does thing\n\n**FIELDS:**\n\n**apiVersion** <string>\n\nThe version\nof the API", explainer.formatExplain("DESCRIPTION:\nKind does thing\n\nFIELDS:\n  apiVersion <string>\n    The version\n    of the API"));
-        assert.equal("**DESCRIPTION:**\nKind does\nthing\n\n**FIELDS:**\n\n**apiVersion** <string>\n\nThe version\nof the API\n\n**kind** <string>\n\nKind is kind", explainer.formatExplain("DESCRIPTION:\nKind does\nthing\n\nFIELDS:\n  apiVersion <string>\n    The version\n    of the API\n\n  kind <string>\n    Kind is kind"));
-    });
-
-    test("Resources are transformed correctly", function() {
-        assert.equal("**RESOURCE:** spec <object>\n\n**DESCRIPTION:**\nThis is\na description\n\nAnd this is a less indented\npart of the description\n\n**FIELDS:**\n\n**apiVersion** <string>\n\nThe version\nof the API\n\n**volumes** <[]object>\n\nVolumes are voluminous", explainer.formatExplain("RESOURCE: spec <object>\n\nDESCRIPTION:\n     This is\n     a description\n\n    And this is a less indented\n    part of the description\n\nFIELDS:\n   apiVersion <string>\n     The version\n     of the API\n\n   volumes <[]object>\n     Volumes are voluminous"));
-    });
-
-    test("Windows line breaks are handled correctly", function() {
-        assert.equal("**RESOURCE:** spec <object>\n\n**DESCRIPTION:**\nThis is\na description\n\nAnd this is a less indented\npart of the description\n\n**FIELDS:**\n\n**apiVersion** <string>\n\nThe version\nof the API\n\n**volumes** <[]object>\n\nVolumes are voluminous", explainer.formatExplain("RESOURCE: spec <object>\r\n\r\nDESCRIPTION:\r\n     This is\r\n     a description\r\n\r\n    And this is a less indented\r\n    part of the description\r\n\r\nFIELDS:\r\n   apiVersion <string>\r\n     The version\r\n     of the API\r\n\r\n   volumes <[]object>\r\n     Volumes are voluminous"));
-    });
-
-    test("Required fields are emphasised", function() {
-        assert.equal("**RESOURCE:** spec <object>\n\n**DESCRIPTION:**\nFoo\n\n**FIELDS:**\n\n**containers** <[]Object>  **[required]**\n\nSome containers", explainer.formatExplain("RESOURCE: spec <object>\n\nDESCRIPTION:\n     Foo\n\nFIELDS:\n   containers <[]Object>  -required-\n     Some containers"));
-    });
 
     var swaggerJson = fs.readFileSync(path.join(__dirname, 'kube-swagger.json'), 'utf8');
     var swagger = JSON.parse(swaggerJson);
