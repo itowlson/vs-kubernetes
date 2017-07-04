4f4564b230ce48ec55972aa7fe589f4791ae46b7
Ivan Towlson <ivan.towlson@microsoft.com>
2017-07-04 16:14:23 +1200

    Initial commit of 'draft up' support

--- src/draft.ts
+++ src/draft.ts
@@ -1,12 +1,14 @@
 import { Host } from './host';
-import { Shell } from './shell';
+import { Shell, ShellResult } from './shell';
 import { FS } from './fs';
-import * as path from 'path';
+import * as syspath from 'path';
 
 export interface Draft {
     checkPresent() : Promise<boolean>;
-    isFolderMapped(dirPath: string) : boolean;
+    isFolderMapped(path: string) : boolean;
     packs() : Promise<string[] | undefined>;
+    invoke(args: string) : Promise<ShellResult>;
+    path() : Promise<string | undefined>;
 }
 
 export function create(host : Host, fs : FS, shell : Shell) : Draft {
@@ -18,11 +20,12 @@ interface Context {
     readonly fs : FS;
     readonly shell : Shell;
     draftFound : boolean;
+    draftPath : string;
 }
 
 class DraftImpl implements Draft {
     constructor(host : Host, fs : FS, shell : Shell, draftFound : boolean) {
-        this.context = { host : host, fs : fs, shell : shell, draftFound : draftFound };
+        this.context = { host : host, fs : fs, shell : shell, draftFound : draftFound, draftPath : 'draft' };
     }
 
     private readonly context : Context;
@@ -31,12 +34,20 @@ class DraftImpl implements Draft {
         return checkPresent(this.context);
     }
 
-    isFolderMapped(dirPath: string) : boolean {
-        return isFolderMapped(this.context, dirPath);
+    isFolderMapped(path: string) : boolean {
+        return isFolderMapped(this.context, path);
     }
 
     packs() : Promise<string[] | undefined> {
         return packs(this.context);
+    }
+
+    invoke(args: string) : Promise<ShellResult> {
+        return invoke(this.context, args);
+    }
+
+    path() : Promise<string | undefined> {
+        return path(this.context);
     }
 }
 
@@ -53,12 +64,35 @@ async function packs(context : Context) : Promise<string[] | undefined> {
         const dhResult = await context.shell.exec("draft home");
         if (dhResult.code === 0) {
             const draftHome = dhResult.stdout.trim();
-            const draftPacksDir = path.join(draftHome, 'packs');
+            const draftPacksDir = syspath.join(draftHome, 'packs');
             const draftPacks = context.fs.dirSync(draftPacksDir);
             return draftPacks;
         }
     }
 
+    return undefined;
+}
+
+async function invoke(context : Context, args : string) : Promise<ShellResult> {
+    if (await checkPresent(context)) {
+        const result = context.shell.exec(context.draftPath + ' ' + args);
+        return result;
+    }
+}
+
+// TODO: Windows-isation is similar to kubectl module
+async function path(context : Context) : Promise<string | undefined> {
+    let bin = await pathCore(context);
+    if (context.shell.isWindows() && bin && !(bin.endsWith('.exe'))) {
+        bin = bin + '.exe';
+    }
+    return bin;
+}
+
+async function pathCore(context : Context) : Promise<string | undefined> {
+    if (await checkPresent(context)) {
+        return context.draftPath;
+    }
     return undefined;
 }
 
@@ -83,7 +117,9 @@ async function checkForDraftInternal(context : Context) : Promise<boolean> {
 
     context.draftFound = context.fs.existsSync(bin);
 
-    if (!context.draftFound) {
+    if (context.draftFound) {
+        context.draftPath = bin;
+    } else {
         alertNoDraft(context, 'configuredFileMissing', bin + ' does not exist!');
     }
 
@@ -143,9 +179,9 @@ async function findBinary(context : Context, binName : string) : Promise<FindBin
 
 // END TODO
 
-function isFolderMapped(context: Context, dirPath: string) : boolean {
+function isFolderMapped(context: Context, path: string) : boolean {
     // Heuristic based on files created by 'draft create'
-    const tomlFile = path.join(dirPath, '.draftignore');
-    const ignoreFile = path.join(dirPath, 'draft.toml');
+    const tomlFile = syspath.join(path, '.draftignore');
+    const ignoreFile = syspath.join(path, 'draft.toml');
     return context.fs.existsSync(tomlFile) && context.fs.existsSync(ignoreFile);
 }
